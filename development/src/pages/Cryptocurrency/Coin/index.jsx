e0710dfb3b37e3785b4f7a9114169fef16a07f46
Sazonov Alexey <sazonleha010203@gmail.com>
2022-09-20 05:06:40 +0300

    layout CoinInfo

--- development/src/pages/Cryptocurrency/Coin/index.jsx
+++ development/src/pages/Cryptocurrency/Coin/index.jsx
@@ -2,7 +2,16 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { Box, Typography, CircularProgress } from "@mui/material";
+import {
+  Box,
+  Typography,
+  CircularProgress,
+  Paper,
+  Chip,
+  Divider,
+  Link,
+  Avatar,
+} from "@mui/material";
 
 function Coin({ id }) {
   const navigate = useNavigate();
@@ -29,15 +38,101 @@ function Coin({ id }) {
       sx={{
         display: "flex",
         flexDirection: "column",
-        justifyContent: "center",
-        minHeight: "93vh",
+        justifyContent: loading ? "start" : "center",
+        minHeight: loading ? "auto" : "93vh",
         width: "1",
-        alignItems: loading ? "start" : "center",
+        alignItems: "center",
         border: "1px solid green",
-        px: { xs: 1, sm: 2, md: 3, lg: 4, xl: 5 },
+        px: { xs: 2, sm: 4, md: 7, lg: 12, xl: 15 },
       }}
     >
-      {loading ? data.description.en : <CircularProgress size="5rem" />}
+      {loading ? (
+        <Paper
+          sx={{
+            py: 3,
+            px: { xs: 3, sm: 3, md: 5, lg: 8, xl: 10 },
+            width: 1,
+            marginTop: 1,
+            mx: { xs: 2, sm: 3, md: 5, lg: 8, xl: 10 },
+            maxWidth: "1300px",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              width: 1,
+              alignItems: "center",
+            }}
+          >
+            <Box>
+              <Chip label={data.market_cap_rank} variant="outlined" />
+              <Chip
+                label={data.symbol}
+                variant="outlined"
+                sx={{
+                  borderRadius: 2,
+                  ml: { xs: 0.5, md: 1 },
+                }}
+              />
+            </Box>
+            <Typography
+              sx={{
+                fontWeight: 600,
+                textAlign: "center",
+                mx: 1,
+              }}
+            >
+              {data.name}
+            </Typography>
+            <Link
+              href={data.links.homepage[0]}
+              rel="noopener"
+              underline="none"
+              variant="body1"
+              target="_blank"
+              sx={{
+                pl: 6,
+              }}
+            >
+              site
+            </Link>
+          </Box>
+          <Divider
+            sx={{
+              my: 1,
+            }}
+          />
+          <Box
+            sx={{
+              display: "grid",
+              gridTemplateColumns: { xs: "1fr", md: "30% 1fr" },
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                p: 2,
+              }}
+            >
+              <Avatar
+                src={data.image.large}
+                sx={{
+                  width: "1",
+                  height: "min-content",
+                }}
+                variant="square"
+              />
+              <Divider sx={{ width: "1px", mx: 1 }} orientation="vertical" />
+            </Box>
+            <Box sx={{
+              p: 2,
+            }}> {data.description.en}</Box>
+          </Box>
+        </Paper>
+      ) : (
+        <CircularProgress size="5rem" />
+      )}
     </Box>
   );
 }
