97801c94d200eef02b2a4275ac79a798197f3df3
AlexeySazonov31 <sazonleha010203@gmail.com>
2023-06-22 18:20:35 +0300

    start cur inputs render

--- app-dev/src/pages/Currencies/index.jsx
+++ app-dev/src/pages/Currencies/index.jsx
@@ -4,21 +4,61 @@ import { useError } from "../Error/ErrorContext";
 import {
     Paper,
     Box,
-    ToggleButtonGroup, 
-    ToggleButton, 
-    FormControl, 
-    InputLabel, 
-    Select, 
-    MenuItem, 
+    ToggleButtonGroup,
+    ToggleButton,
+    FormControl,
+    InputLabel,
+    Select,
+    MenuItem,
     IconButton,
-    Typography
+    Typography,
+    TextField,
+    FilledInput,
+    InputAdornment,
 } from "@mui/material";
 
 function Currencies() {
     const [data, setData] = useState(null);
     const [convertCur, setConvertCur] = useState("USD");
 
-    const primaryCur = "BLR";
+//     const [inputData, setInputData] = useState([
+//         {
+//             iso: "BYN",
+//             value: "",
+//         },
+//         {
+//             iso: "EUR",
+//             value: "",
+//         },
+//         {
+//             iso: "USD",
+//             value: "",
+//         },
+//         {
+//             iso: "PLN",
+//             value: "",
+//         },
+// ]);
+const inputData = [
+            {
+                iso: "BYN",
+                value: "",
+            },
+            {
+                iso: "EUR",
+                value: "",
+            },
+            {
+                iso: "USD",
+                value: "",
+            },
+            {
+                iso: "PLN",
+                value: "",
+            },
+    ];
+
+    const primaryCur = "BYN";
 
     const { showError } = useError();
 
@@ -34,7 +74,7 @@ function Currencies() {
             })
     }, []);
 
-    console.log(data)
+    console.log(inputData)
 
     function showRate(cur) {
         return (data.find((elem) => { return elem.iso === cur }));
@@ -42,53 +82,80 @@ function Currencies() {
 
     console.log(data)
 
-    const listCur = data ? (data.map((elem, index) => {
-        return <MenuItem key={index} value={elem.iso}>{elem.iso}</MenuItem>
-    })) : <></>;
-
-    console.log(listCur);
 
     const changeconvertCur = (event) => {
         setConvertCur(event.target.value);
-      };
-    
-    return data ? (<Paper
-        sx={{
-            width: 0.5,
-            padding: 2,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-        }}
-    >
-        <Box
-            sx={{
-                display: "flex",
-                flexDirection: "row",
-                width: 1,
-                mt: 1
-            }}
-        >
-            <FormControl sx={{ width: 0.5 }} size="small">
-                <InputLabel id="demo-select-small">currency</InputLabel>
-                <Select
-                    labelId="demo-select-small"
-                    id="demo-select-small"
-                    name="name"
-                    defaultValue="USD"
-                    value={convertCur}
-                    label="Age"
-                    onChange={changeconvertCur}
-                >
-{listCur}
-                </Select>
-            </FormControl>
-        </Box>
-        <Typography variant="h5">
-             {`${showRate(convertCur).quantity} ${convertCur} = ` + showRate(convertCur).rate + ` ${primaryCur}`}
-        </Typography>
+    };
 
-    </Paper>) : (<></>)
+
+
+    return data ? (
+        <Box sx={{ width: 1, display: "flex", justifyContent: "center", alignItems: "center", minHeight: "80vh" }}>
+            <Paper
+                sx={{
+                    width: 0.5,
+                    height: "fit-content",
+                    padding: 2,
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center"
+                }}
+            >
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        width: 0.7,
+                        mt: 1
+                    }}
+                >
+                    <FormControl sx={{ width: 1 }} size="small">
+                        <InputLabel id="demo-select">currency</InputLabel>
+                        <Select
+                            labelId="demo-select-small"
+                            id="outlined-select-currency-native"
+                            name="name"
+                            defaultValue="USD"
+                            value={convertCur}
+                            label="currency"
+                            onChange={changeconvertCur}
+                            sx={{my: 1}}
+                        >
+                            {data.map((menuItem) => (
+                                <MenuItem key={menuItem.iso} value={menuItem.iso}>
+                                    {menuItem.iso}
+                                </MenuItem>
+                            ))}
+                        </Select>
+
+
+                    </FormControl>
+
+
+                
+                </Box>
+                {inputData.map((option) => {
+                                    <Typography>{option.iso}</Typography>
+                                    {/* <FormControl fullWidth sx={{ m: 1 }} variant="filled">
+                                    <InputLabel htmlFor="filled-adornment-amount">Amount</InputLabel>
+                                    <FilledInput
+                                      id="filled-adornment-amount"
+                                      startAdornment={<InputAdornment position="start">{elem.iso}</InputAdornment>}
+                                    />
+                                    {elem.value}
+                                  </FormControl> */}
+                                  
+                    })}
+                                                        <Typography>{inputData[0].iso}</Typography>
+
+                <Typography variant="h5">
+                    {`${showRate(convertCur).quantity} ${convertCur} = ` + showRate(convertCur).rate + ` ${primaryCur}`}
+                </Typography>
+
+            </Paper>
+        </Box>
+    ) : (<></>)
 }
 
 
